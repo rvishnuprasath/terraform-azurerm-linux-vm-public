6564582a6bed89a6e17412d6adbe4dbd6801428e
Vishnuprasath  R <vishnuprasath.r@quadrasystems.net>
2025-12-16 13:54:54 +0530

    TF Modules Files 8th Commit

--- modules/linux-vm/7.outputs.tf
+++ modules/linux-vm/7.outputs.tf
@@ -8,15 +8,27 @@ output "resource_group_name" {
   value = azurerm_resource_group.myrg.name # Argument Reference
 }
 
+/*
 # 3. Output Values - Virtual Machine Public IP Address
 output "vm_public_ip_address" {
   description = "My Virtual Machine Public IP"
   value       = azurerm_linux_virtual_machine.mylinuxvm.public_ip_address
 }
 
+
 # 4. Output Values - Virtual Machine Username
 output "vm_username" {
   description = "VM USERNAME"
   value       = azurerm_linux_virtual_machine.mylinuxvm.admin_username
 }
+*/
+
+output "vm_public_ip_address" {
+  value = azurerm_linux_virtual_machine.mylinuxvm.public_ip_address
+}
+
+output "vm_username" {
+  value = azurerm_linux_virtual_machine.mylinuxvm.admin_username
+}
+
 
